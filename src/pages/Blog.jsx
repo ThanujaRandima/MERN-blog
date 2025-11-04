b14e11fc21f0dd0df7c085ccf9573888d9be22f5
Thanuja Randima <thanujaeuw@gmail.com>
2025-11-04 15:34:54 +0530

    blog - comment section

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { assets, blog_data } from '../assets/assets';
+import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment';
 
@@ -10,15 +10,23 @@ const Blog = () => {
   const {id} = useParams();
 
   const [data, setData] = useState(null);
+  const [comments, setComments] = useState([]);
 
   const fetchBlogData = async () => {
     const data = blog_data.find(item => item._id === id);
     setData(data)
   }
 
+  const fetchComments = async () => {
+    setComments(comments_data)
+  }
+
   useEffect(() => {
-    fetchBlogData();
+    fetchBlogData()
+    fetchComments()
   }, [])
+
+ 
 
   return data ?  (
     <div className='relative'>
@@ -40,6 +48,27 @@ const Blog = () => {
             className='rich-text max-w-3xl mx-auto'>
 
         </div>
+
+        {/* comment section */}
+        <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+          <p>Comments ({comments.length}) </p>
+          <div className='flex flex-col gap-4'>
+            {comments.map((item, index) => (
+              <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-3xl p-4 rounded text-gray-600'>
+
+                <div className='flex items-center gap-2 mb-2'>
+                  <img src={assets.user_icon} alt="" className='w-6'/>
+                  <p className='font-medium'>{item.name}</p>
+                </div>
+
+                <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+
+              </div>
+            ))}
+          </div>
+        </div>
+
       </div>
 
       
